e1cd54e2b315345cd4f9c7f1b70e24e766ad2ef6
HTML <HTML@server.ipartekaula.com>
2024-07-10 11:27:20 +0200

    Ejemplo composición de clases (Local)

---
+++ oop/src/oop/pojos/Local.java
@@ -0,0 +1,100 @@
+package oop.pojos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Local {
+	private Long id;
+	private String nombre;
+	private Persona propietario;
+	private Integer capacidad;
+	
+	private ArrayList<Persona> visitantes = new ArrayList<>();
+
+	public Local(Long id, String nombre, Persona propietario, Integer capacidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.propietario = propietario;
+		this.capacidad = capacidad;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Persona getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(Persona propietario) {
+		this.propietario = propietario;
+	}
+
+	public Integer getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(Integer capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public List<Persona> getVisitantes() {
+		return Collections.unmodifiableList(visitantes);
+	}
+	
+	// MÉTODOS DE INSTANCIA
+	public void entrar(Persona persona) {
+		if(persona == null) {
+			throw new IllegalArgumentException("No se admiten visitantes nulos");
+		}
+		
+		if(visitantes.size() >= capacidad) {
+			throw new RuntimeException("No cabe nadie más");
+		}
+		
+		visitantes.add(persona);
+	}
+	
+	public void salir(Persona persona) {
+		visitantes.remove(persona);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(capacidad, id, nombre, propietario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Local other = (Local) obj;
+		return Objects.equals(capacidad, other.capacidad) && Objects.equals(id, other.id)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(propietario, other.propietario);
+	}
+
+	@Override
+	public String toString() {
+		return "Local [id=" + id + ", nombre=" + nombre + ", propietario=" + propietario + ", capacidad=" + capacidad
+				+ "]";
+	}
+}
---
+++ oop/src/oop/pruebas/LocalPruebas.java
@@ -0,0 +1,28 @@
+package oop.pruebas;
+
+import oop.pojos.Local;
+import oop.pojos.Persona;
+
+public class LocalPruebas {
+	public static void main(String[] args) {
+		Persona propietario = new Persona("Javier");
+		Local local = new Local(1L, "Ipartek", propietario, 3);
+		
+		System.out.println(local);
+		
+		local.entrar(new Persona("Pepe"));
+		local.entrar(new Persona("Juan"));
+		local.entrar(new Persona("Pedro"));
+		// local.entrar(new Persona("Julio"));
+		
+		System.out.println(local.getVisitantes());
+		
+		// local.getVisitantes().add(null);
+		
+		local.getVisitantes().get(1).setNombre("Juanito");
+		
+		for(Persona visitante: local.getVisitantes()) {
+			System.out.println(visitante);
+		}
+	}
+}
