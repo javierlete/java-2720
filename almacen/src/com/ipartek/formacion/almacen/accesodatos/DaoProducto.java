7a321ba51d4bc753fc2866f7e4814ffb220f67b1
HTML <HTML@server.ipartekaula.com>
2024-07-15 11:26:14 +0200

    Definimos las operaciones de acceso a datos concretas para productos

---
+++ almacen/src/com/ipartek/formacion/almacen/accesodatos/DaoProducto.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.almacen.accesodatos;
+
+import java.math.BigDecimal;
+
+import com.ipartek.formacion.almacen.entidades.Producto;
+
+public interface DaoProducto extends Dao<Producto> {
+	default Iterable<Producto> productosPorPrecio(BigDecimal minimo, BigDecimal maximo) {
+		throw new AccesoDatosException("NO ESTÁ IMPLEMENTADO");
+	}
+}
