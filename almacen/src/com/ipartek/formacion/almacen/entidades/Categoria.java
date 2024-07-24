bb7c6597c46a8d401518ce1a89ecd0515908384a
HTML <HTML@server.ipartekaula.com>
2024-07-24 11:19:11 +0200

    Anotamos las clases con JPA

--- almacen/src/com/ipartek/formacion/almacen/entidades/Categoria.java
+++ almacen/src/com/ipartek/formacion/almacen/entidades/Categoria.java
@@ -1,25 +1,51 @@
 package com.ipartek.formacion.almacen.entidades;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
-import jakarta.validation.constraints.Min;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
+
+@Entity
+@Table(name = "categorias")
 public class Categoria {
-	@Min(0)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotNull
 	@NotBlank
+	@Size(max = 50)
 	private String nombre;
 
 	@NotBlank
+	@Lob
+	@Size(max = 2000)
 	private String descripcion;
 
-	private final ArrayList<Producto> productos = new ArrayList<>();
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	@OneToMany(mappedBy = "categoria")
+	private Set<Producto> productos;
+	
+	public Categoria(Long id, String nombre, String descripcion) {
+		this(id, nombre, descripcion, new HashSet<>());
+	}
 }
--- almacen/src/com/ipartek/formacion/almacen/entidades/Producto.java
+++ almacen/src/com/ipartek/formacion/almacen/entidades/Producto.java
@@ -4,24 +4,39 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
+
+@Entity
+@Table(name = "productos")
 public class Producto implements Serializable {
 
 	private static final long serialVersionUID = -5259817006588696574L;
 
-	@Min(0)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@NotNull
 	@NotBlank
+	@Size(max = 50)
 	private String nombre;
 	
 	@NotNull
@@ -32,8 +47,10 @@ public class Producto implements Serializable {
 	private Integer stock;
 	
 	@FutureOrPresent(message = "debe ser posterior o igual a la actual")
+	@Column(name = "fecha_caducidad")
 	private LocalDate fechaCaducidad;
 	
+	@ManyToOne
 	private Categoria categoria;
 	
 	public Producto(Long id, String nombre, BigDecimal precio, Integer stock, LocalDate fechaCaducidad) {
