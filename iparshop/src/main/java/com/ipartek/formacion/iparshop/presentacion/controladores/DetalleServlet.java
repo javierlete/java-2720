11caf9da8cdfbc8ec68bc41e673e6759c3b41af3
HTML <HTML@server.ipartekaula.com>
2024-09-03 09:59:04 +0200

    Simulacro de detalle

---
+++ iparshop/src/main/java/com/ipartek/formacion/iparshop/presentacion/controladores/DetalleServlet.java
@@ -0,0 +1,19 @@
+package com.ipartek.formacion.iparshop.presentacion.controladores;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/detalle")
+public class DetalleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("detalle.jsp").forward(request, response);
+	}
+
+}
---
+++ iparshop/src/main/webapp/detalle.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="jakarta.tags.core" prefix="c" %>
+<%@ taglib uri="jakarta.tags.fmt" prefix="fmt" %>
+<fmt:setLocale value="es-ES"/>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>IparShop</title>
+</head>
+<body>
+
+	<h1>IparShop</h1>
+	
+	<dl>
+		<dt>Nombre</dt>
+		<dd>Producto 1</dd>
+		<dt>Precio</dt>
+		<dd>1.234,56 €</dd>
+	</dl>
+
+</body>
+</html>
