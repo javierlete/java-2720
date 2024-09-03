10fb66c0626c1919b46d843747269c7524e7c4c7
HTML <HTML@server.ipartekaula.com>
2024-09-03 11:45:17 +0200

    Excepción de acceso a datos y utilidades de JPA (copiar y pegar)

---
+++ iparshop/src/main/java/com/ipartek/formacion/iparshop/accesodatos/AccesoDatosException.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.iparshop.accesodatos;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class AccesoDatosException extends RuntimeException {
+	private static final long serialVersionUID = -361533206882619063L;
+}
