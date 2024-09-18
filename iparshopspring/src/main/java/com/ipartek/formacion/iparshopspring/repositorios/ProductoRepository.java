79721cc586f048d3b71a87eb830e7bd8c36d4015
HTML <HTML@server.ipartekaula.com>
2024-09-18 11:46:44 +0200

    API REST automática con paginación y ordenación

--- iparshopspring/src/main/java/com/ipartek/formacion/iparshopspring/repositorios/ProductoRepository.java
+++ iparshopspring/src/main/java/com/ipartek/formacion/iparshopspring/repositorios/ProductoRepository.java
@@ -3,10 +3,13 @@ package com.ipartek.formacion.iparshopspring.repositorios;
 import java.math.BigDecimal;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.iparshopspring.entidades.Producto;
 
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
+@RepositoryRestResource(path = "productos", collectionResourceRel = "productos")
+public interface ProductoRepository extends CrudRepository<Producto, Long>, PagingAndSortingRepository<Producto, Long> {
 	Iterable<Producto> findByNombreContaining(String nombre);
 	
 	Iterable<Producto> findByPrecioBetween(BigDecimal minimo, BigDecimal maximo);
