3d1e7d1ed0725b5388f5d175b08945c763c541fc
HTML <HTML@server.ipartekaula.com>
2024-09-18 10:18:39 +0200

    Spring Starter Project: Lombok, JPA, Validation, MySQL

---
+++ iparshopspring/src/main/java/com/ipartek/formacion/iparshopspring/IparshopspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.iparshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IparshopspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IparshopspringApplication.class, args);
+	}
+
+}
---
+++ iparshopspring/src/test/java/com/ipartek/formacion/iparshopspring/IparshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.iparshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IparshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
