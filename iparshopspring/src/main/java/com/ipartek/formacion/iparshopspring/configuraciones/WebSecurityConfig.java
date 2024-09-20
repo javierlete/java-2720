a7f32ac87cda7a5489da82e5d8ff99977ed3e2ac
HTML <HTML@server.ipartekaula.com>
2024-09-20 11:08:21 +0200

    Seguridad con autenticación hardcodeada y rutas con roles

---
+++ iparshopspring/src/main/java/com/ipartek/formacion/iparshopspring/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.ipartek.formacion.iparshopspring.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	// AUTENTICACIÓN
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails javier =
+			 User.withDefaultPasswordEncoder()
+				.username("javier@email.net")
+				.password("javier")
+				.roles("ADMIN")
+				.build();
+	
+		UserDetails pepe =
+				 User.withDefaultPasswordEncoder()
+					.username("pepe@email.net")
+					.password("pepe")
+					.roles("USER")
+					.build();
+		
+		return new InMemoryUserDetailsManager(javier, pepe);
+	}
+
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/usuario/**").hasRole("USER")
+				.anyRequest().permitAll() //.authenticated()
+			)
+			.formLogin((form) -> form
+//				.loginPage("/login")
+				.permitAll()
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+}
