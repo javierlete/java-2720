d09013ba4120d90632dde5d18e044d0ea6a39c62
HTML <HTML@server.ipartekaula.com>
2024-09-23 10:09:32 +0200

    Gestión de usuarios y contraseñas con tablas por defecto

--- iparshopspring/src/main/java/com/ipartek/formacion/iparshopspring/configuraciones/WebSecurityConfig.java
+++ iparshopspring/src/main/java/com/ipartek/formacion/iparshopspring/configuraciones/WebSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.ipartek.formacion.iparshopspring.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,23 +17,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
 	// AUTENTICACIÓN
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication()
+	    	.dataSource(dataSource);
+	}
+
 	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails javier =
-			 User.withDefaultPasswordEncoder()
-				.username("javier@email.net")
-				.password("javier")
-				.roles("ADMIN")
-				.build();
-	
-		UserDetails pepe =
-				 User.withDefaultPasswordEncoder()
-					.username("pepe@email.net")
-					.password("pepe")
-					.roles("USER")
-					.build();
-		
-		return new InMemoryUserDetailsManager(javier, pepe);
+	PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 
 	// AUTORIZACIÓN
