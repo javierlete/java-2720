c01fa3315c5d325e84daafbc50193043aeadd0a1
HTML <HTML@server.ipartekaula.com>
2024-09-24 09:43:58 +0200

    Inicio IparTube

---
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/IpartubeApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ipartube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartubeApplication.class, args);
+	}
+
+}
---
+++ ipartube/src/test/java/com/ipartek/formacion/ipartube/IpartubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ipartube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
