034ffcb1b7f702a2ca7f2708d1ccc8c33b441b1b
HTML <HTML@server.ipartekaula.com>
2024-09-24 11:04:07 +0200

    Servicio básico de video

---
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/servicios/AnonimoService.java
@@ -0,0 +1,7 @@
+package com.ipartek.formacion.ipartube.servicios;
+
+import com.ipartek.formacion.ipartube.entidades.Video;
+
+public interface AnonimoService {
+	Iterable<Video> videosPrincipal();
+}
