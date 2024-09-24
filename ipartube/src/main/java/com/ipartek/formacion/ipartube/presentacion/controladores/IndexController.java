57e0d5b780175fd26cb709ea5b86bc8dc5bc9b84
HTML <HTML@server.ipartekaula.com>
2024-09-24 11:04:27 +0200

    Controlador modificado para usar el servicio

--- ipartube/src/main/java/com/ipartek/formacion/ipartube/presentacion/controladores/IndexController.java
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/presentacion/controladores/IndexController.java
@@ -1,14 +1,22 @@
 package com.ipartek.formacion.ipartube.presentacion.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ipartek.formacion.ipartube.servicios.AnonimoService;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+	@Autowired
+	private AnonimoService anonimoService;
+	
 	@GetMapping
-	public String index() {
+	public String index(Model modelo) {
+		modelo.addAttribute("videos", anonimoService.videosPrincipal());
 		return "index";
 	}
 }
