96700ef96b8e45fdbd3a363420ddb2e14536c7f5
HTML <HTML@server.ipartekaula.com>
2024-09-24 11:04:15 +0200

    Implementación del servicio

---
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/servicios/AnonimoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ipartek.formacion.ipartube.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ipartek.formacion.ipartube.entidades.Video;
+import com.ipartek.formacion.ipartube.repositorios.VideoRepository;
+
+@Service
+public class AnonimoServiceImpl implements AnonimoService {
+	@Autowired
+	private VideoRepository videoRepository;
+	
+	@Override
+	public Iterable<Video> videosPrincipal() {
+		return videoRepository.findAll();
+	}
+
+}
